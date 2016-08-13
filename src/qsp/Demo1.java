b831fbc4dccf92f56584ac03f13c19cdda31aa9a
Suganthi <suganthic4u@gmail.com>
2016-08-13 18:33:58 +0530

    commit

--- src/qsp/Demo1.java
+++ src/qsp/Demo1.java
@@ -4,10 +4,19 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
+
 public class Demo1 {
 @Test
 public void testA() {
 //System.setProperty("webdriver.gecko.driver","C:/Users/CBT/Desktop/Suganthi/geckodriver.exe");
+ExtentReports e=new ExtentReports("./ExtentReport/extentreport.html");
+ExtentTest t = e.startTest("testA");
+t.log(LogStatus.PASS,"Hi");
+e.endTest(t);
+e.flush();
 WebDriver driver=new FirefoxDriver();
 driver.get("https://www.google.com");
 driver.close();
