d2d15c4f264a5efc49a46f2ee316c6d95529ea4e
Suganthi <suganthic4u@gmail.com>
2016-08-14 14:03:02 +0530

    comitt

--- src/qsp/Demo1.java
+++ src/qsp/Demo1.java
@@ -2,6 +2,7 @@ package qsp;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Reporter;
 import org.testng.annotations.Test;
 
 import com.relevantcodes.extentreports.ExtentReports;
@@ -9,16 +10,24 @@ import com.relevantcodes.extentreports.ExtentTest;
 import com.relevantcodes.extentreports.LogStatus;
 
 public class Demo1 {
+	ExtentReports e=new ExtentReports("./ExtentReport/extentreport.html");
+	ExtentTest t = e.startTest("testA");
 @Test
 public void testA() {
 //System.setProperty("webdriver.gecko.driver","C:/Users/CBT/Desktop/Suganthi/geckodriver.exe");
-ExtentReports e=new ExtentReports("./ExtentReport/extentreport.html");
-ExtentTest t = e.startTest("testA");
-t.log(LogStatus.PASS,"Hi");
-e.endTest(t);
-e.flush();
 WebDriver driver=new FirefoxDriver();
 driver.get("https://www.google.com");
-driver.close();
+Reporter.log("Open Google",true);
+t=e.startTest("test");
+t.log(LogStatus.PASS,"Hi");
+e.endTest(t);
+}
+@Test
+public void testB()
+{
+	t=e.startTest("test");
+	t.log(LogStatus.FAIL, "Bye");
+	e.endTest(t);
+	e.flush();
 }
 }
